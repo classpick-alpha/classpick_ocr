da41e35ec0c183937467fdbe5d6137c5a1eb3837
Son Daehyeon <sondaehyeon01@gmail.com>
2025-05-15 15:37:17 +0900

    feat(fastapi): 오류 헨들러 구현

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from datetime import datetime, timezone, timedelta
 import cv2
 import numpy as np
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from paddleocr import PaddleOCR
 
@@ -13,38 +14,53 @@ ocr = PaddleOCR(use_angle_cls=True, lang="korean")
 KST = timezone(timedelta(hours=9))
 
 
-@app.post('/')
-async def ocr_endpoint(file: UploadFile = File(...)):
-    try:
-        image_bytes = await file.read()
-        image_array = np.frombuffer(image_bytes, np.uint8)
-        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-        if image is None:
-            raise ValueError("이미지를 디코딩할 수 없습니다.")
-
-        response = []
-        for item in ocr.ocr(image, cls=True)[0]:
-            box_points, (text, score) = item
-            response.append({
-                "text": text,
-                "score": score,
-                "box": [{"x": int(point[0]), "y": int(point[1])} for point in box_points],
-            })
-
-        return JSONResponse(status_code=200, content={
+def response(status_code: int, *, error: str = None, data=None):
+    return JSONResponse(
+        status_code=200,
+        content={
             "timestamp": datetime.now(KST).strftime('%Y-%m-%d %H:%M'),
-            "status": 200,
-            "message": None,
-            "data": response,
-        })
-    except Exception as e:
-        return JSONResponse(status_code=200, content={
-            "timestamp": datetime.now(KST).strftime('%Y-%m-%d %H:%M'),
-            "status": 500,
-            "message": str(e),
-            "data": None,
-        })
+            "status": status_code,
+            "message": error,
+            "data": data,
+        }
+    )
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(_: Request, e: HTTPException):
+    return response(e.status_code, error=e.detail)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(_: Request, e: RequestValidationError):
+    error_detail = e.errors()[0].get("msg", "Validation error")
+    return response(400, error=error_detail)
+
+
+@app.exception_handler(Exception)
+async def unhandled_exception_handler(_: Request, e: Exception):
+    return response(500, error=str(e))
+
+
+@app.post("/")
+async def ocr_endpoint(request: Request):
+    image_bytes = await request.body()
+    image_array = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    if image is None:
+        raise Exception("이미지를 디코딩할 수 없습니다.")
+
+    ocr_result = ocr.ocr(image, cls=True)[0]
+
+    if ocr_result is None:
+        return response(200, data=[])
+
+    return response(200, data=list(map(lambda x: {
+        'text': x[1][0],
+        'score': x[1][1],
+        'box': [{"x": int(point[0]), "y": int(point[1])} for point in x[0]]
+    }, ocr_result)))
 
 
 if __name__ == "__main__":
